b237ffb2fd9523197debbd3976180272415177e8
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2024-05-29 11:14:38 +0100

    Add operate function and MAX_DECIMALS constant

--- js-file.js
+++ js-file.js
@@ -1,44 +1,69 @@
+const MAX_DECIMALS = 15;
+
 function checkIfNumbers(a,b) {
     return (typeof(+a) === 'number' && typeof(+b) === 'number');
 }
 
 function add(a,b) {
-    return checkIfNumbers(a,b) ? parseFloat((+a + +b).toFixed(15)) : "ERROR";
+    return checkIfNumbers(a,b) ? parseFloat((+a + +b).toFixed(MAX_DECIMALS)) : "ERROR";
 }
 
 function subtract(a,b) {
-    return checkIfNumbers(a,b) ? parseFloat((+a - +b).toFixed(15)) : "ERROR";
+    return checkIfNumbers(a,b) ? parseFloat((+a - +b).toFixed(MAX_DECIMALS)) : "ERROR";
 }
 
 function multiply(a,b) {
-    return checkIfNumbers(a,b) ? parseFloat((+a * +b).toFixed(15)) : "ERROR";
+    return checkIfNumbers(a,b) ? parseFloat((+a * +b).toFixed(MAX_DECIMALS)) : "ERROR";
 }
 
 function divide(a,b) {
-    return checkIfNumbers(a,b) ? parseFloat((+a / +b).toFixed(15)) : "ERROR";
+    return checkIfNumbers(a,b) ? parseFloat((+a / +b).toFixed(MAX_DECIMALS)) : "ERROR";
 }
 
-console.log("Add");
-console.log(add(1,2));
-console.log(add(4,2));
-console.log(add(1,-2));
+function operate(a,b,op) {
+    if(checkIfNumbers(a,b)) {
+        switch (op) {
+            case '+':
+                add(a,b);
+                break;
+            case '-':
+                subtract(a,b);
+                break;
+            case '*':
+                multiply(a,b);
+                break;
+            case '/':
+                divide(a,b);
+                break;
+        }   
+    }
+}
 
-console.log("Subtract");
-console.log(subtract(1,2));
-console.log(subtract(4,2));
-console.log(subtract(1,-2));
+// console.log("Add");
+// console.log(add(1,2));
+// console.log(add(4,2));
+// console.log(add(1,-2));
 
-console.log("Multiply");
-console.log(multiply(1,2));
-console.log(multiply(4,2));
-console.log(multiply(1,-2));
-console.log(multiply(-1,-2));
+// console.log("Subtract");
+// console.log(subtract(1,2));
+// console.log(subtract(4,2));
+// console.log(subtract(1,-2));
 
-console.log("Divide");
-console.log(divide(1,2));
-console.log(divide(4,2));
-console.log(divide(1,-2));
-console.log(divide(-1,-2));
-console.log(divide(3,7));
-console.log(divide(7,3));
-console.log(add(divide(3,7),divide(7,3)));
+// console.log("Multiply");
+// console.log(multiply(1,2));
+// console.log(multiply(4,2));
+// console.log(multiply(1,-2));
+// console.log(multiply(-1,-2));
+
+// console.log("Divide");
+// console.log(divide(1,2));
+// console.log(divide(4,2));
+// console.log(divide(1,-2));
+// console.log(divide(-1,-2));
+// console.log(divide(3,7));
+// console.log(divide(7,3));
+// console.log(add(divide(3,7),divide(7,3)));
+
+let firstNumber;
+let operator;
+let secondNumber;
