d1762a0081b80c01c0b2742b67e8d4d6d2eb0ab9
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2024-05-29 18:22:28 +0100

    Finished calculation logic and displaying correct number

--- js-file.js
+++ js-file.js
@@ -1,33 +1,102 @@
-const MAX_DECIMALS = 15;
+const MAX_DECIMALS = 8;
 const buttons = document.querySelectorAll('button');
 const buttonsArray = Array.from(buttons);
 const screen = document.querySelector("#screen");
+
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let result = null;
+let moreThanOneOperator = false;
 
 function initializeEventListeners() {
 
     buttonsArray.forEach((button) => {
         if(button.className === "number") {
-            button.addEventListener("click", () => {
-                if(screen.textContent === '0') {
-                    screen.textContent = button.textContent;
-                } else {
-                    screen.textContent += button.textContent;
-                }
+            button.addEventListener("click", (e) => {
+                updateDisplay(e);
             });
         }
 
         if(button.className === "clear") {
-            button.addEventListener("click", () => {
-                screen.textContent = '0';
+            button.addEventListener("click", (e) => {
+                resetVariables();
+                updateDisplay(e);
             });
         }
 
         if(button.className === "operator") {
-            button.addEventListener("click", () => {
-                screen.textContent = '0';
+            button.addEventListener("click", (e) => {
+                handleOperator(e);
+                updateDisplay(e);
+            });
+        }
+
+        if(button.className === "equals") {
+            button.addEventListener("click", (e) => {
+                handleEquals();
+                updateDisplay(e);
             });
         }
     });
+}
+
+function resetVariables() {
+    firstNumber = null;
+    operator = null;
+    secondNumber = null;
+    result = null;
+    moreThanOneOperator = false;
+}
+
+function updateDisplay(e) {
+    if (e.target.className === "number") {
+        let button = e.target;
+
+        if(screen.textContent === '0' || moreThanOneOperator) {
+            screen.textContent = button.textContent;
+            moreThanOneOperator = false;
+        } else {
+            screen.textContent += button.textContent;
+        }
+    }
+
+    if (e.target.className === "clear") {
+        screen.textContent = 0;
+    }
+
+    if (e.target.className === "operator") {
+        if(result) {
+            screen.textContent = result;
+            moreThanOneOperator = true;
+        } else {
+            screen.textContent = 0;
+        }
+    }
+
+    if (e.target.className === "equals") {
+        screen.textContent = result;
+    }
+
+    screen.textContent = screen.textContent.substring(0, 10);
+}
+
+function handleOperator(e) {
+    if(!operator) {
+        operator = e.target.textContent;
+        firstNumber = screen.textContent;
+    } else {
+        //Resolve the previous operator
+        handleEquals();
+        operator = e.target.textContent;
+        firstNumber = result;
+    }
+}
+
+function handleEquals() {
+    secondNumber = screen.textContent;
+    result = operate(firstNumber, secondNumber, operator);
+    operator = null;
 }
 
 function checkIfNumbers(a,b) {
@@ -65,34 +134,5 @@ function operate(a,b,op) {
     }
 }
 
-let firstNumber;
-let operator;
-let secondNumber;
-
 initializeEventListeners();
-
-// console.log("Add");
-// console.log(add(1,2));
-// console.log(add(4,2));
-// console.log(add(1,-2));
-
-// console.log("Subtract");
-// console.log(subtract(1,2));
-// console.log(subtract(4,2));
-// console.log(subtract(1,-2));
-
-// console.log("Multiply");
-// console.log(multiply(1,2));
-// console.log(multiply(4,2));
-// console.log(multiply(1,-2));
-// console.log(multiply(-1,-2));
-
-// console.log("Divide");
-// console.log(divide(1,2));
-// console.log(divide(4,2));
-// console.log(divide(1,-2));
-// console.log(divide(-1,-2));
-// console.log(divide(3,7));
-// console.log(divide(7,3));
-// console.log(add(divide(3,7),divide(7,3)));
 
