beebea17556e0386241c489f0e50c190af792d8b
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2024-05-29 11:01:27 +0100

    Added subtract function

--- js-file.js
+++ js-file.js
@@ -1,9 +1,21 @@
-function add(a,b) {
-    if(typeof(a) === 'number' && typeof(b) === 'number') {
-        return a + b;
-    }
+function checkIfNumbers(a,b) {
+    return (typeof(+a) === 'number' && typeof(+b) === 'number');
 }
 
+function add(a,b) {
+    return checkIfNumbers(a,b) ? +a + +b : "ERROR";
+}
+
+function subtract(a,b) {
+    return checkIfNumbers(a,b) ? +a - +b : "ERROR";
+}
+
+console.log("Add");
 console.log(add(1,2));
 console.log(add(4,2));
 console.log(add(1,-2));
+
+console.log("Subtract");
+console.log(subtract(1,2));
+console.log(subtract(4,2));
+console.log(subtract(1,-2));
